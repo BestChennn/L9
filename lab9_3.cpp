4733c6f15fb4541aa331fb5c171480772aaeb513
BestChennn <bestchenbestchen@gmail.com>
2021-12-06 16:17:08 +0700

    Complete

--- lab9_3.cpp
+++ lab9_3.cpp
@@ -1,11 +1,108 @@
-/*
-"Enter your age: "
-"Enter your height: "
-"Enter your property: "
-"Your status = "
-"UNFRIEND"
-"FRIEND"
-"BEST FRIEND"
-"ONE-NIGHT-STAND"
-"MARRIED"
-*/
+#include<iostream>
+#include<string>
+
+using namespace std;
+int main(){
+
+    int age ;
+    int height;
+    int pp;
+    string stt ;
+
+    cout << "Enter your age: ";
+    cin >> age ;
+    if(age <= 20){
+        cout << "Enter your height: ";
+        cin >> height ;
+        if(height < 160){
+            stt = "UNFRIEND";
+            
+        }else{
+            if(height < 175 and height >= 160){
+                stt = "FRIEND";
+            }else{
+                cout <<"Enter your property: ";
+                cin >> pp;   
+                if(pp > 69000000){
+                    stt = "MARRIED";
+                }else{
+                    stt = "ONE-NIGHT-STAND";
+                }
+            }
+
+
+        }
+    }else{
+        if(age < 30){
+            cout <<"Enter your property: ";
+            cin >> pp;
+            if(pp > 10000000){
+                stt = "BEST FRIEND";
+            }else{
+                stt ="UNFRIEND";
+             }
+        }else{
+            stt="UNFRIEND";
+        }
+
+    }
+
+    cout << "Your status = " << stt <<"\n";
+
+        return 0 ;
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
